d7de90642882cb1142f063bb87c89dc21da9dd97
Nicholas Spyrison <spyrison@gmail.com>
2019-01-21 23:05:07 +1100

    diagram and remote hosting of PDF sense fig 8 reproduction.

--- _figures.r
+++ _figures.r
@@ -1,40 +1,41 @@
-# Make figures
+rep(print("THIS WAS INITIAL DOCUMENT AND MAY BE STALE. CHAPTER CODE SHOULD BE TRUSTED OVER THIS."),3)
+
 library(spinifex)
 library(ggplot2)
 library(gridExtra)
 set.seed(1) #don't think this matters, but can't hurt.
 
-## FLEA HOLES TOUR
-flea_std <- tourr::rescale(tourr::flea[,1:6])
-tpath    <- tourr::save_history(flea_std, tourr::guided_tour(tourr::holes))
-h_bas  <- matrix(tpath[,,max(dim(tpath)[3])], ncol=2)
+# ## FLEA HOLES TOUR
+# flea_std <- tourr::rescale(tourr::flea[,1:6])
+# hpath    <- tourr::save_history(flea_std, tourr::guided_tour(tourr::holes))
+# h_bas    <- matrix(hpath[,,max(dim(hpath)[3])], ncol=2)
+#
+# #0_left
+# step0_l <- view_basis(h_bas) # Maybe manual tour on v1?
+#
+# h_m_sp <- create_manip_space(h_bas, manip_var = 1)
+# h_dat  <- cbind(data.frame(flea_std %*% h_m_sp[, 1:2]), flea$species)
+# colnames(h_dat) <- c("x", "y", "species")
+#
+# #0_right
+# step0_r <- ggplot() +
+#   geom_point(h_dat, mapping = aes(x=x, y=y, color=species)) +
+#   scale_color_brewer(palette = "Dark2") +
+#   theme_void() +
+#   theme(legend.position=c(0.8, 0.8)) +
+#   theme(legend.background = element_rect(colour = 'black', fill = 'grey90', linetype='solid'))
+#
+#
+# #=== Step 0 out
+# step0 <- grid.arrange(step0_l, step0_r, ncol=2)
+# ggsave("./output/step0_basis+proj.png", step0,
+#        height = 4, width = 4*1.61, units = "in")
 
-run <- TRUE
-if (run) {
-  #1_left
-  (step1_l <- view_basis(h_bas)) # Maybe manual tour on v1?
+#=== Step 2 out
+step2 <- view_manip_sp(h_bas, 5)
+ggsave("./output/step2_manip_sp.png", step2,
+       width = 4, height = 4, units = "in")
 
-  h_m_sp <- create_manip_space(h_bas, manip_var = 1)
-  h_dat  <- cbind(data.frame(flea_std %*% h_m_sp[, 1:2]), flea$species)
-  colnames(h_dat) <- c("x", "y", "species")
-
-  #1_right
-  (step1_r <- ggplot() +
-      geom_point(h_dat, mapping = aes(x=x, y=y, color=species)) +
-      scale_color_brewer(palette = "Dark2") +
-      theme_void() +
-      theme(legend.position=c(0.8, 0.8)) +
-      theme(legend.background = element_rect(colour = 'black', fill = 'grey90', linetype='solid'))
-  )
-
-  #=== Step 1 out
-  (step1 <- grid.arrange(step1_l, step1_r, ncol=2))
-  ggsave("./output/step1_basis+proj.png", step1, width = 4*1.61, height = 4)
-
-  #=== Step 2 out
-  (step2 <- view_manip_sp(h_bas, 5))
-  ggsave("./output/step2_manip_sp.png", step2, width = 4, height = 4)
-}
 
 #===
 mtour <- manual_tour(h_bas, manip_var = 5, n_slides = 15)
@@ -70,10 +71,6 @@ circ_grid <- merge(x = circ, y = grid, by = NULL) # CROSS JOIN,
   geom_text(aes(x = V1+x, y = V2+y, label = lab_abbr),
             color = col_v, vjust = "outward", hjust = "outward") +
   geom_path(data = circ_grid, mapping = aes(x = x+c_x, y = y+c_y), color = "grey80") +
-  theme_void()) #+
+  theme_void())
 ggsave("./output/step3_manual_tour.png", step3, width = 4*(5/3), height = 4)
-
-
-?geom_text
-
 
