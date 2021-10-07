3faae8838bbdbd8317c0dac4ed5741c722ce1820
Nick Spyrison <spyrison@gmail.com>
2021-10-07 14:27:26 +1100

    lots of work on chapter 4 figures

--- figures_from_script/_ch4_efficacy_radial_tour.r
+++ figures_from_script/_ch4_efficacy_radial_tour.r
@@ -311,10 +311,219 @@ ggsave(
 # ch4_tab2_model_coefficients -----
 ## Handled inline, see `04-efficacy_radial_tour.Rmd`
 
-# ch4_fig5_subjestive_measures -----
-# ... not yet done? manually transfer?
+# Setup2 ----
 
-# ch4_fig6_rand_effect_size ----
-# include? I think it really supports the idea of using mixxed models.
+require("ggpubr")
+my_theme <- list(
+  theme_bw(),
+  scale_color_brewer(palette = "Dark2"),
+  scale_fill_brewer(palette = "Dark2"),
+  geom_hline(yintercept = 0L),
+  theme(legend.position = "bottom",
+        legend.box = "vertical",
+        legend.margin = margin(-6))
+)
+my_ggpubr <- function(
+  df, x = "factor", y = "value", title = waiver(), subtitle = waiver(),
+  lab.y_coef = "auto"){ ## Alternatively 1 + (n_levels+1)*.1
+  ## Find height of global significance test text.
+  .x_lvls <- df %>% pull({{x}}) %>% levels()
+  .y_range <- diff(range(df[y]))
+  .n_lvls <- length(.x_lvls)
+  if(lab.y_coef == "auto")
+    lab.y_coef = 1 + (.n_lvls+2) *.1
+  .lab.y_global <- min(df[y]) + (lab.y_coef) * .y_range
+    #(.04 * .y_range) * (1 + .n_lvls) * .y_range + max(df[y])
+  my_comparisons <- list(c("pca", "grand"), c("grand", "radial"), c("pca", "radial"))
+
+  ## Plot
+  ggviolin(df, x = x, y = y, fill = x, alpha = .6,
+           palette = "Dark2", shape = x,
+           add = c("mean"), ## Black circle, can change size, but not shape or alpha?
+           draw_quantiles = c(.25, .5, .75)) +
+    stat_compare_means(method = "wilcox.test",
+                       comparisons = my_comparisons,
+                       label = "p.signif",
+                       hide.ns = TRUE) + ## pairwise test
+    # stat_compare_means(label = "p.signif", label.y = .lab.y - .4,
+    #                    method = "wilcox.test", ref.group = .x_lvls[1]) + ## Test each lvl w.r.t. first level.
+    stat_compare_means( ## Global test
+      label.y = .lab.y_global,
+      aes(label = paste0("p=", ..p.format..))
+    ) + ## custom label
+    my_theme +
+    ggtitle(title, subtitle)
+}
+my_ggpubr_facet <- function(..., facet = "measure"){
+  facet(my_ggpubr(...), facet.by = facet)
+}
+
+
+# ch4_fig5_ABcd_violins TODO -----
+{
+  dat_qual <- readRDS("./data/dat_qual.rds")
+  .lvls <- c("0/100%", "33/66%", "50/50%")
+  dat_qual$location <-
+    factor(.lvls[as.integer(dat_qual$location)], levels = .lvls)
+  .no_legend <- theme(legend.position = "off")
+  .factor <- my_ggpubr(dat_qual, x = "factor", y = "marks") +
+    .no_legend
+  .location <- my_ggpubr(dat_qual, x = "location", y = "marks") +
+    .no_legend
+  .shape <- my_ggpubr(dat_qual, x = "shape", y = "marks") +
+    .no_legend
+  .dim <- my_ggpubr(dat_qual, x = "dim", y = "marks") +
+    .no_legend
+  .FactorLocation <-
+    my_ggpubr_facet(dat_qual, x = "factor", y = "marks",
+                    facet = "location") + .no_legend
+
+  title <- cowplot::ggdraw() +
+    cowplot::draw_label("Violin plots of the terms in marks^ = \u03b1 * \u03b2 + \u03b3 + \u03b4",
+                        x = .5, y = .75, hjust = .5, vjust = 1)
+  top <- cowplot::plot_grid(
+    .factor, .location, .shape, .dim, nrow = 1)
+  gc()
+  (ABcd_violins <-
+      cowplot::plot_grid(title, top, .FactorLocation + ggtitle("", ""),
+                         ncol = 1, rel_heights = c(.1, 1, 1.4)))
+}
+ggsave("./figures_from_script/ch4_fig5_ABcd_violins.pdf",
+       ABcd_violins, device = cairo_pdf,
+       width = .w, height = .w, unit = .u)
+# ch4_figX_demographic_heatmap -----
+require("tidyverse")
+require("dplyr")
+## Load aggregated data. filter to only surveys in the 108 instances in the analysis
+survey_wider <- readRDS("./data/survey_wider.rds")
+instance_id_whitelist <- readRDS("./data/instance_id_whitelist.rds")
+## Only Prolific participants that were in the 108 instance_ids in the analysis
+survey_wider <- survey_wider %>%
+  dplyr::filter(nchar(as.character(prolific_id)) == 24,
+         survey_wider$instance_id %in% instance_id_whitelist)
+str(survey_wider)
+
+## Change character to factor, include counts in the levels of sex?
+(demographic_heatmap <- ggplot(survey_wider, aes(education, age)) +
+    stat_bin2d(aes(fill = after_stat(count))) +
+    geom_text(aes(label = after_stat(count)), stat = "bin2d") +
+    facet_grid(cols = vars(pronoun), labeller = label_wrap_gen(width=18)) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1),
+          # legend.position = "bottom",
+          # legend.direction = "horizontal",
+          legend.margin = margin(0, 0, 0, 0)) +
+    scale_fill_gradient(low = "lightpink", high = "firebrick", na.value = NA) +
+    ggtitle("Participant demographics"))
+if(F)
+  ggsave(filename = "./figures_from_script/ch4_figX_demographic_heatmap.pdf",
+         plot = demographic_heatmap, device = "pdf", width = .w, height = .w/2)
+
+# ch4_fig6_subjective_measures -----
+## Follow the loose setup of _analysis.rmd:
+.u = "in"
+.w = 6.25
+.h = 9
+.l_lvls <- c("most disagree", "disagree", "neutral", "agree", "most agree")
+
+survey_wider <- readRDS("./data/survey_wider.rds")
+str(survey_wider)
+
+{ ## Subjective violins munging
+  ## pivot_longer within factor
+  radial_longer <- survey_wider %>%
+    dplyr::select(instance_id, radial_familar:radial_like) %>%
+    tidyr::pivot_longer(radial_familar:radial_like,
+                        names_to = "factor", values_to = "value")
+  grand_longer <- survey_wider %>%
+    dplyr::select(instance_id, grand_familar:grand_like) %>%
+    tidyr::pivot_longer(grand_familar:grand_like,
+                        names_to = "factor", values_to = "value")
+  pca_longer <- survey_wider %>%
+    dplyr::select(instance_id, pca_familar:pca_like) %>%
+    tidyr::pivot_longer(pca_familar:pca_like,
+                        names_to = "factor", values_to = "value")
+  ## Combine and split measure from factor
+  subjective_longer <- rbind(radial_longer, grand_longer, pca_longer) %>%
+    tidyr::separate(factor, c("factor", "measure"), sep = "_")
+}
+
+{ ## Subjective violins
+  .lvls <- c("most disagree", "disagree", "neutral", "agree", "most agree")
+  subjective_longer <- subjective_longer %>%
+    mutate(value = as.integer(plyr::mapvalues(value, from = .lvls, to = 1L:5L)),
+           measure = factor(plyr::mapvalues(measure,
+                                            from = c("like", "ease", "confidence", "familar"),
+                                            to = c("preference", "ease of use", "confidence", "familiarity"))),
+           factor = factor(factor, levels = c("pca", "grand", "radial"))
+    )
+  (subjective_violins <- my_ggpubr_facet(df = subjective_longer, x = "factor", y = "value"))
+}
+
+{ ## Subjective violins munging
+  col_idx <- 8:22
+  col_nms <- colnames(survey_wider[, col_idx])
+  survey_agg <- tibble()
+  mute <- sapply(col_nms, function(col_nm){
+    tmp <- survey_wider[col_nm] %>%
+      group_by_all() %>%
+      count() %>%
+      as.data.frame()
+    .this_agg <- data.frame(question = col_nm,
+                            response = tmp[, 1],
+                            n = tmp[, 2],
+                            percent = 100 * tmp[, 2] / sum(tmp[, 2]))
+    survey_agg <<- rbind(survey_agg, .this_agg)
+  })
+  str(survey_agg)
+
+  ## Format likert questions
+  likert_q_nms <- colnames(survey_wider[, 11:22])
+  likert <<- survey_agg %>% filter(question %in% likert_q_nms) %>%
+    tidyr::separate(question, c("factor", "question"), sep = "_") %>%
+    mutate(factor = factor(factor, levels = rev(c("pca", "grand", "radial"))),
+           response = factor(response, levels = rev(.l_lvls)))
+  likert$question <-
+    plyr::mapvalues(likert$question,
+                    from = c("like", "ease", "confidence", "familar"),
+                    to = c("preference", "ease of use", "confidence", "familiarity")) %>%
+    factor()
+  str(likert)
+}
+
+## Likert plots -----
+(subjective_likert <-
+    ggplot(likert, aes(x = percent, y = factor, fill = response)) +
+    geom_bar(position = "fill", stat = "identity", width = .6) + facet_grid(vars(question)) +
+    ggtitle("Subjective measures",
+            "Likert scale [1-5]") +
+    theme_bw() +
+    scale_fill_manual(values = rev(RColorBrewer::brewer.pal(5, "PRGn"))) +
+    theme(legend.position = "bottom",
+          legend.direction = "horizontal") +
+    # Reverse order that fill is displayed in legend.
+    guides(fill = guide_legend(reverse = TRUE)) +
+    # x as % rather than rate.
+    scale_x_continuous(labels = scales::percent)
+)
+
+## SAVING ----
+## Cowplot and bringing it together
+require("cowplot")
+figSubjectiveMeasures <-
+  cowplot::plot_grid(subjective_likert +
+                       coord_flip() +
+                       theme(legend.direction = "vertical") +
+                       guides(fill = guide_legend(reverse = FALSE)),
+                     subjective_violins,
+                     ncol = 2)
+
+ggsave("./figures_from_script/ch4_fig6_subjective_measures.pdf",
+       figSubjectiveMeasures, device = "pdf",
+       width = .w, height = .w, units = "in")
+
+
+# ch4_fig7_rand_effect_size ----
+# include? I think it really supports the idea of using mixed models.
 
 
