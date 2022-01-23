b563be5a2bac87f61ee4fdb6aad24fdd9bc1d9ec
Nick Spyrison (Acer laptop) <spyrison@gmail.com>
2022-01-23 18:13:26 +1100

    Adding MT algorithm: -best version with algorithm & algpseudocode - work on intro/background - fix spinifex figure legends

--- figures/_ch3_spinifex.r
+++ figures/_ch3_spinifex.r
@@ -1,6 +1,4 @@
 # Setup ------
-if(F)
-  remotes::install_github("nspyrison/spinifex")
 require(spinifex) ## Current dev ver, likely a small diff from CRAN spinifex v0.3.1
 require(tourr)
 require(ggplot2)
@@ -10,8 +8,8 @@ require(dplyr)
 # fig1_biplot -----
 
 ## Flea holes tour
-set.seed(20190425) ## Doesn't change tourr output.
-f_dat <- tourr::rescale(flea[,1:6])
+set.seed(20190425) ## Doesn't make tourr output reproducible, but a hope.
+f_dat <- tourr::rescale(flea[, 1:6])
 f_clas <- factor(flea$species)
 ## Hard code a basis. tourr doesn't fit results with set.seed().
 f_bas <- c(.693, -.022, .082, -.119, .706, .023,
@@ -21,11 +19,11 @@ f_bas <- c(.693, -.022, .082, -.119, .706, .023,
 rownames(f_bas) <- colnames(f_dat)
 
 biplot <- ggtour(f_bas, f_dat) +
-  proto_default(list(color = f_clas, shape = f_clas))
+  proto_default(aes_args = list(color = f_clas, shape = f_clas))
 
 ggplot2::ggsave(
   "./figures/ch3_fig1_biplot.pdf", biplot, "pdf",
-  height=2, scale=1, units="in")
+  height = 2, scale = 1, units = "in")
 
 # fig2_manip_sp -----
 f_mvar <- 5
@@ -33,19 +31,19 @@ manip_sp <- spinifex::view_manip_space(basis = f_bas, manip_var = f_mvar)
 
 ggplot2::ggsave(
   "./figures/ch3_fig2_manip_sp.pdf", manip_sp, "pdf",
-  height=2, scale=1, units="in")
+  height = 2, scale = 1, units = "in")
 
 
 # fig3_filmstrip -----
 ## All arguments
 mt <- manual_tour(basis = f_bas, manip_var = f_mvar)
 ggt <- ggtour(mt, f_dat, angle = 3) +
-  proto_default(list(color = f_clas, shape = f_clas))
+  proto_default(aes_args = list(color = f_clas, shape = f_clas))
 film <- filmstrip(ggt)
 
 ggplot2::ggsave(
   "./figures/ch3_fig3_filmstrip.pdf", film, "pdf",
-  width=8, scale=1, units="in")
+  width = 8, scale = 1, units = "in")
 
 
 
@@ -59,7 +57,7 @@ jet_clas  <- factor(JetCluster_sub$exp)
 jet_mvar  <- 4
 jet_ang   <- .315 ## gives 12 frames
 jet_mtour <- manual_tour(basis = jet_bas, manip_var = jet_mvar)
-ggt <- ggtour(jet_mtour, jet_dat, angle = pi*3) +
+ggt <- ggtour(jet_mtour, jet_dat, angle = pi * 3) +
   proto_default(list(color = jet_clas, shape = jet_clas),
                 list(alpha = .5))
 fig4_jet_better_pc4 <- filmstrip(ggt)
@@ -67,7 +65,7 @@ fig4_jet_better_pc4 <- filmstrip(ggt)
 ggplot2::ggsave(
   "./figures/ch3_fig4_jet_better_pc4.pdf",
   fig4_jet_better_pc4, "pdf",
-  width=8, scale=1, units="in")
+  width = 8, scale = 1, units = "in")
 
 #fig5_jet_worse_pc3 -----
 jet_mvar  <- 3
@@ -80,7 +78,7 @@ fig5_jet_worse_pc3 <- filmstrip(ggt)
 ggplot2::ggsave(
   "./figures/ch3_fig5_jet_worse_pc3.pdf",
   fig5_jet_worse_pc3, "pdf",
-  width=8, scale=1, units="in")
+  width = 8, scale = 1, units = "in")
 
 # fig6_DIS_better_pc6 -----
 load("./data/grDIScenter.rda")
@@ -95,23 +93,23 @@ DIS_ang   <- .32
 DIS_mtour <- manual_tour(basis = DIS_bas, manip_var = DIS_mvar)
 
 ggt <- ggtour(DIS_mtour, DIS_dat, angle = pi*3) +
-  proto_default(list(color = DIS_clas, shape = DIS_clas),
-                list(alpha = .5))
+  proto_default(aes_args = list(color = DIS_clas, shape = DIS_clas),
+                identity_args = list(alpha = .5))
 fig6_DIS_better_pc6 <- filmstrip(ggt)
 ggplot2::ggsave(
   "./figures/ch3_fig6_DIS_better_pc6.pdf",
   fig6_DIS_better_pc6, "pdf",
-  width=8, scale=1, units="in")
+  width = 8, scale = 1, units="in")
 
 # fig7_DIS_worse_pc2 -----
 DIS_mvar <- 2
 DIS_mtour <- manual_tour(basis = DIS_bas, manip_var = DIS_mvar)
 
 ggt <- ggtour(DIS_mtour, DIS_dat, angle = pi*3) +
-  proto_default(list(color = DIS_clas, shape = DIS_clas),
-                list(alpha = .5))
+  proto_default(aes_args = list(color = DIS_clas, shape = DIS_clas),
+                identity_args = list(alpha = .5))
 fig7_DIS_worse_pc2 <- filmstrip(ggt)
 ggplot2::ggsave(
   "./figures/ch3_fig7_DIS_worse_pc2.pdf",
   fig7_DIS_worse_pc2, "pdf",
-  width=8, scale=1, units="in")
+  width = 8, scale = 1, units="in")
