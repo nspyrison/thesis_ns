f529e797e819ac536b32f5601ba9210f13551b2d
Nick Spyrison (Acer laptop) <spyrison@gmail.com>
2022-01-27 18:50:25 +1100

    Address Kim's 27 Jan comment and clean up figures a bit.

--- figures/_ch1_introduction.r
+++ figures/_ch1_introduction.r
@@ -10,28 +10,28 @@ bas <- basis_olda(X, Y, 4)
 
 left <- ggtour(bas[, c(3,4)], X, 0) +
   proto_point(list(color = Y, shape = Y)) +
-  proto_basis() + theme(legend.position = "off")
+  proto_basis(line_size = .6) + theme(legend.position = "off")
 right <- ggtour(bas[, c(1,2)], X, 0) +
     proto_point(list(color = Y, shape = Y)) +
-    proto_basis() + theme(legend.position = "off")
+    proto_basis(line_size = .6) + theme(legend.position = "off")
 (cp <- cowplot::plot_grid(left, right))
 
 ggsave("./figures/ch1_fig2_penguin_cl_sep.png", cp, device = "png",
-       width = 6, height = 2.2, units = "in")
+       width = 6, height = 2, units = "in")
 
 
 ## penguin_gt_filmstrip -----
-set.seed(2022) ## doesnt seem to impact interpolate.
+set.seed(2022) ## doesnt seem to impact tourr::interpolate, but wishful thinking
 gt_path <- save_history(X, max_bases = 3)
 dim(interpolate(gt_path, angle = .6))
 ## Stochastic!?? this is supposed to be geodesic interpolation should not vary...
 
 ggt <- ggtour(gt_path, X, .75) +
   proto_point(list(color = Y, shape = Y)) +
-  proto_basis() + theme(legend.position = "off")
+  proto_basis(line_size = .6) + theme(legend.position = "off")
 (fs <- filmstrip(ggt, ncol = 3))
 ggsave("./figures/ch1_fig3_penguin_grandtour.png", fs, device = "png",
-       width = 6, height = 3.3, units = "in")
+       width = 6, height = 3, units = "in")
 
 
 
@@ -41,8 +41,8 @@ mt_path <- manual_tour(bas, 1, data = X)
 
 ggt <- ggtour(mt_path, X, 99) +
   proto_point(list(color = Y, shape = Y)) +
-  proto_basis() + theme(legend.position = "off")
+  proto_basis(line_size = .6) + theme(legend.position = "off")
 (fs <- filmstrip(ggt, ncol = 3))
 ggsave("./figures/ch1_fig4_penguin_manualtour.png", fs, device = "png",
-       width = 6, height = 2.2, units = "in")
+       width = 6, height = 2, units = "in")
 
